00cdafd795df4269d14920a85d32f9b58ede876f
Benjamin Bezias <benjaminbezias@gmail.com>
2023-03-29 09:38:19 +0100

    fix logger

--- src/logger.ts
+++ src/logger.ts
@@ -3,13 +3,11 @@ import { createLogger, transports, format } from 'winston';
 export const logger = createLogger({
 	transports: [new transports.Console()],
 	format: format.combine(
-		format.errors({stack: true}),
 		format.colorize(),
 		format.timestamp(),
 		format.printf(({ timestamp, level, message }) => {
 			return `[${timestamp}] ${level}: ${message}`;
-		}),
-		format.prettyPrint()
+		})
 	),
 });
 
